861961fcf695573ae22e10766e8940dc86f6cb9c
timfam <tim.anyasi@gmail.com>
2015-05-16 17:33:34 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Description
+This [Meteor](https://www.meteor.com/) package is a plugin for
+[CSSC](https://atmospherejs.com/pierreeric/cssc).
+
+Please refer to [CSSC](https://atmospherejs.com/pierreeric/cssc) for details.
+
+It provides bootstrap styling
+
+
+## Installation
+```bash
+meteor add pierreeric:cssc-bootstrap
+```
---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  summary: 'Bootstrap plugin for CSSC',
+  version: '0.0.1',
+  name: 'timfam:cssc-bootstrap',
+  // git: 'https://github.com/PEM--/cssc-bootstrap.git'
+});
+
+Package.onUse(function(api) {
+  api.use(['coffeescript', 'pierreeric:cssc@1.0.4',]);
+  api.addFiles('cssc-bootstrap.coffee', 'client');
+  api.imply('pierreeric:cssc');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('timfam:cssc-bootstrap');
+  api.addFiles('cssc-bootstrap-tests.js');
+});
